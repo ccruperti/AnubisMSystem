275dfe9dcb5e505df6f46a5b2f073db513fc6f2b
aguilar996 <aguilar996@hotmail.com>
2019-11-22 12:28:52 -0500

    Update CustomAuthorizationFilter.cs

--- AnubisDBMS/AnubisDBMS.Infraestructure/Filters/WebFilters/CustomAuthorizationFilter.cs
+++ AnubisDBMS/AnubisDBMS.Infraestructure/Filters/WebFilters/CustomAuthorizationFilter.cs
@@ -54,6 +54,7 @@ namespace AnubisDBMS.Infraestructure.Filters.WebFilters
 
         private bool IsAuthorized(AuthorizationContext filterContext)
         {
+
             var descriptor = filterContext.ActionDescriptor;
             var authorizeAttr = descriptor.GetCustomAttributes(typeof(AuthorizeAttribute), false).FirstOrDefault() as AuthorizeAttribute;
  
